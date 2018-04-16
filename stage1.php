e859aacabbdb954c6ccb79a999a7acb66b0d662b
aghwotu <aghwotuovuoke@gmail.com>
2018-04-16 02:06:01 +0100

    Initial commit

---
+++ stage1.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG4 stage 1</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<style type="text/css">
+		body{
+
+			background: url("images/crocus.jpg") top right no-repeat;
+			background-attachment: fixed;
+			background-size: 100%;
+			width: 200px;
+			font-family: 'Lato', Calibri, Arial, sans-serif;
+		}
+
+		h1{
+			position: absolute;
+			width: 595px;
+			height: 162px;
+			left: 953px;
+			top: 225px;
+
+			/*font-family: Roboto;*/
+			font-style: normal;
+			font-weight: normal;
+			line-height: normal;
+			font-size: 96px;
+
+			color: #F2F2F2;
+			margin-top: -100px;
+		}
+
+
+		h2{
+			position: absolute;
+			width: 746px;
+			height: 102px;
+			left: 694px;
+			top: 336px;
+
+			/*font-family: Roboto;*/
+			font-style: normal;
+			font-weight: normal;
+			line-height: normal;
+			font-size: 45px;
+
+			color: #F2F2F2;
+
+			margin-top: -70px;
+			margin-left: 90px;
+			line-height: 6px;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<h1>HNG4 </h1>
+			<h2>Friday, 13th April 05:47am</h2>
+		</div>
+		
+
+	</div>
+	
+
+</body>
+</html>
